15de88fd318f7ec718ca5bc9f9b3301b7ef21fa2
Sigurður Sveinn Halldórsson <siggisv@gmail.com>
2022-08-15 17:22:56 +0200

    Update tmux_scripts/pygame_dev.sh
    Even though I'm not using this setup any more, then it's better to fix
    some minor details to make it play better with other sessions (like not
    using the global flag where it should not be used and specify the
    session when selecting windows/panes).

--- tmux_scripts/pygame_dev.sh
+++ tmux_scripts/pygame_dev.sh
@@ -7,43 +7,43 @@ if [ $? != 0 ]; then
 	cd /home/siggisv/work/projects/pygame-dev/pygame
 	tmux new-session -s $SESSION_NAME -n "build/test" -d
 	tmux set -g default-terminal "tmux-256color"
-	tmux set -g status-left-length 16
-	tmux set -g status-left "|#[bg=yellow]#S#[default]| "
-	tmux set -g status-justify centre
-	tmux send-keys ". ../venv-pygame/bin/activate" C-m
-	tmux send-keys clear C-m
+	tmux select-window -t $SESSION_NAME:0
+
+	tmux set status-left-length 16
+	tmux set status-left "|#[bg=yellow]#S#[default]| "
+	tmux set status-justify centre
+
+	tmux select-pane -t $SESSION_NAME:0.0 -T "Testing"
+	tmux send-keys ". ../venv-pygame/bin/activate" C-m "clear" C-m
+
 	tmux split-window -h
-	tmux send-keys ". ../venv-linting/bin/activate;clear" C-m
-	tmux send-keys C-m
-	tmux send-keys "echo \"python setup.py docs\"" C-m
+	tmux select-pane -t $SESSION_NAME:0.1 -T "Linting'n'document building"
+	tmux send-keys ". ../venv-linting/bin/activate" C-m "clear" C-m
+	tmux send-keys "python setup.py docs"
+
 	tmux split-window -v
-	tmux send-keys ". ../venv-pygame/bin/activate;clear" C-m
-	tmux send-keys C-m
-	tmux send-keys "echo \"python setup.py build -j2 --debug install\"" C-m
+	tmux select-pane -t $SESSION_NAME:0.2 -T "Build"
+	tmux send-keys ". ../venv-pygame/bin/activate" C-m "clear" C-m
+	tmux send-keys "python setup.py build -j2 --debug install"
+
 	tmux setw pane-border-status top
-	tmux select-pane -t @0.0 -T "Testing"
-	tmux select-pane -t @0.1 -T "Linting'n'document building"
-	tmux select-pane -t @0.2 -T "Build"
-	
-	tmux new-window -n "vim(tests)" -t 1
-	tmux send-key "vim test" C-m
-	
-	tmux new-window -n "vim(main)" -t 2
+
+	tmux new-window -n "vim(tests)" -t $SESSION_NAME:1
+	tmux send-key "vim test"
+
+	tmux new-window -n "vim(main)" -t $SESSION_NAME:2
 	tmux send-key "vim ." C-m
-	
-	tmux new-window -n "git" -t 9
+
+	tmux new-window -n "git" -t $SESSION_NAME:9
 	tmux send-key "git status" C-m
 	tmux split-window -h
 	tmux send-key "git hist --all" C-m
 	tmux split-window -v
 	tmux send-key "git hist --all" C-m
-	
-	tmux new-window -n "grep" -t 8
+
+	tmux new-window -n "grep" -t $SESSION_NAME:8
 	tmux send-key "grep -rn -B5 -A5 --color=always \"Hello\" src_py | less -R" C-m
 	tmux split-window -h
-
-	tmux select-window -t @0
-
 fi
 
-tmux attach -t $SESSION_NAME
+tmux attach -t $SESSION_NAME:0.0
