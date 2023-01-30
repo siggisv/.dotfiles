040fd22ca573d45ac0ef4d21a66c8e9fe0ba8535
Sigurður Sveinn Halldórsson <siggisv@gmail.com>
2023-01-30 15:23:43 +0100

    Set 1st part of bash command prompt to inverse
    I.e. the "username@hostname" part is displayed in inversed colors.

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -10,6 +10,6 @@ GIT_PS1_SHOWUPSTREAM="auto verbose git"
 Venv_color="\[\033[0;32;2;4;7m\]"
 Color_Off="\[\033[0m\]"
 
-TMP_PS1=${PS1%\\$ }
+TMP_PS1="$(tput rev)${PS1%\\$ }"
 
 PROMPT_COMMAND='__git_ps1 "${TMP_PS1}\n${VIRTUAL_ENV:+$Venv_color|`basename $VIRTUAL_ENV`|$Color_Off }" "\$ " "⎇ %s ]"'
