b9ec67a7d48394dcc99d72784163a5e13d304b9c
Sigurður Sveinn Halldórsson <siggisv@gmail.com>
2023-06-28 12:31:58 +0200

    Add and setup autocomplete plugins in neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,21 @@ call plug#begin()
   Plug 'lukas-reineke/indent-blankline.nvim'
   Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 
+  " LSP Support
+  Plug 'neovim/nvim-lspconfig'
+
+  " Autocompletion
+  Plug 'hrsh7th/nvim-cmp'
+  Plug 'hrsh7th/cmp-nvim-lsp'
+  Plug 'hrsh7th/cmp-nvim-lsp-signature-help'
+  Plug 'hrsh7th/cmp-buffer'
+  Plug 'hrsh7th/cmp-path'
+  Plug 'hrsh7th/cmp-cmdline'
+
+  Plug 'L3MON4D3/LuaSnip'         " Required by lsp-zero
+
+  Plug 'VonHeikemen/lsp-zero.nvim', {'branch': 'v2.x'}
+
 call plug#end()
 ]]
 
@@ -51,6 +66,8 @@ vim.keymap.set('', '<C-j>', '<C-W>j')
 vim.keymap.set('', '<C-k>', '<C-W>k')
 vim.keymap.set('', '<C-h>', '<C-W>h')
 vim.keymap.set('', '<C-l>', '<C-W>l')
+
+vim.keymap.set('i', '<c-s>', function() vim.lsp.buf.signature_help() end, {buffer=true})
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.highlight.on_yank() end,
@@ -120,3 +137,78 @@ require'nvim-treesitter.configs'.setup {
     enable = true
   },
 }
+
+-- LSP and autocompletion:
+-- -----------------------
+
+  -- lsp-zero (offers nice defaults for keybindings and integration of
+  -- nvim-cmp with lspconfig)
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- When you don't have mason.nvim installed
+-- You'll need to list the servers installed in your system
+lsp.setup_servers({'clangd'})
+
+-- (Optional) Configure lua language server for neovim
+-- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
+
+  -- nvim-cmp (needs to be setup after lsp-zero)
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+vim.g.cmp_is_enabled = true
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lsp_signature_help'},
+    {name = 'buffer', keyword_length = 2},
+  },
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu (and turn autocompletion on/off)
+    ['<C-Space>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.abort()
+        vim.g.cmp_is_enabled = false
+      else
+        vim.g.cmp_is_enabled = true
+        cmp.complete()
+      end
+    end),
+	},
+  enabled = function()
+    return vim.g.cmp_is_enabled
+  end
+})
+
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      }
+    }
+  })
+})
